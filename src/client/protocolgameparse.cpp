a0e33ef7e53e851115784f8d6a449341b90fb01f
Lucas Chain <sou@lucaschain.com.br>
2026-08-30 14:43:09 -0300

    fix(xibat): refine turret map indicators

--- src/client/protocolgameparse.cpp
+++ src/client/protocolgameparse.cpp
@@ -3874,7 +3874,7 @@ void ProtocolGame::parseCreaturesMark(const InputMessagePtr& msg)
     }
 
     if (squareType == 2) {
-        creature->showStaticSquare(Color::from8bit(squareColor != 0 ? squareColor : 1));
+        creature->showStaticSquare(squareColor == 0 ? Color::black : Color::from8bit(squareColor));
     } else {
         creature->addTimedSquare(squareColor);
     }
