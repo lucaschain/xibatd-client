08be8e9d69a67f707169a447a76f1979bcefb559
Lucas Chain <sou@lucaschain.com.br>
2026-09-12 23:04:47 -0300

    chore(client): update default controls and lighting

--- modules/client_options/data_options.lua
+++ modules/client_options/data_options.lua
@@ -24,7 +24,7 @@ return {
         end
     },
     classicControl                    = {
-        value = g_platform.isMobile() and true or false,
+        value = true,
         action = function(value, options, controller, panels, extraWidgets)
             -- Update the mouseControlMode based on this option
             -- 0 = Regular Controls, 1 = Classic Controls, 2 = Left Smart-Click
@@ -90,7 +90,7 @@ return {
         end
     },
     mouseControlMode                  = {
-        value = 0, -- Default to "Regular Controls"
+        value = 1, -- Default to "Classic Controls"
         action = function(value, options, controller, panels, extraWidgets)
             -- Update the underlying options values first
             -- 0 = Regular Controls, 1 = Classic Controls, 2 = Left Smart-Click
@@ -152,7 +152,7 @@ return {
             end, 50)
         end
     },
-    returnDisablesChat                = false,
+    returnDisablesChat                = true,
     smartWalk                         = false,
     autoChaseOverride                 = true,
     talkOnRightClick                  = false,
@@ -248,7 +248,7 @@ return {
         end
     },
     ambientLight                      = {
-        value = 0,
+        value = 95,
         action = function(value, options, controller, panels, extraWidgets)
             panels.graphicsEffectsPanel:recursiveGetChildById('ambientLight'):setText(string.format(
                 'Ambient light: %s%%', value))
